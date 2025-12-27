889e504492b227c3a35b4066b02745f72e5f2880
Meet Patel <p.meet9305@gmail.com>
2025-12-27 17:11:40 +0530

    updated backend

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const protect = require('../middleware/authMiddleware.js');
 const {
     registerUser,
     loginUser,
@@ -9,7 +10,7 @@ const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-//router.get('/getUser', protect, getUserInfo);
+router.get('/getUser', protect, getUserInfo);
 
 
 module.exports = router;
